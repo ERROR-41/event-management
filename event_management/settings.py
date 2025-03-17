95943665529a5ba626f6392da4bf8f1e0a97bb6e
md.omar <mdomarsadek41@gmail.com>
2025-03-17 17:13:13 +0600

    ready to deploy debug = flase

--- event_management/settings.py
+++ event_management/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-DEBUG = True
+
 
 # Allow only specific hosts
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
