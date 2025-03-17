f0551e0aebca410ccbebb0db988ecbb3a0f7fa8a
md.omar <mdomarsadek41@gmail.com>
2025-03-17 15:13:14 +0600

    adding config

--- event_management/settings.py
+++ event_management/settings.py
@@ -25,6 +25,13 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    config('FRONTEND_URL'),
+]
+
 # Application definition
 
 INSTALLED_APPS = [
