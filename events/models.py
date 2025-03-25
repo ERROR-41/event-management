e9516d25301b1609b48fd0baa167c6b23de23bc4
md.omar <mdomarsadek41@gmail.com>
2025-03-25 10:45:45 +0600

    customuser create

--- events/models.py
+++ events/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from django.db.models import UniqueConstraint
 from cloudinary.models import CloudinaryField
+from django.conf import settings
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -27,14 +29,16 @@ class Event(models.Model):
     ]
     location = models.CharField(max_length=100, choices=LOCATION_CHOICES)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    user_event = models.ManyToManyField(User, through="RSVP", related_name="events")
+    user_event = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, through="RSVP", related_name="events"
+    )
     image = CloudinaryField('image', null=True, blank=True)
 
     def __str__(self):
         return self.name
 
 class RSVP(models.Model):  
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     response = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
--- users/models.py
+++ users/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from cloudinary.models import CloudinaryField
+from django import forms
+
+
+class CustomUser(AbstractUser):
+    phone = models.CharField(max_length=12,default='null',null=True)
+    profile_picture = CloudinaryField("profile", null=True, blank=True)
+    bio = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.username
+    
+    
